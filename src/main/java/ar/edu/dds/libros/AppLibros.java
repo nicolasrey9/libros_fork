c56df7c78f9c9cbe422dd29efa1efe02360fca65
nicolasrey9 <143523417+nicolasrey9@users.noreply.github.com>
2024-10-09 19:27:58 -0300

    Update AppLibros.java

--- src/main/java/ar/edu/dds/libros/AppLibros.java
+++ src/main/java/ar/edu/dds/libros/AppLibros.java
@@ -34,8 +34,8 @@ public class AppLibros {
 		
 		LibrosController controller = new LibrosController(entityManagerFactory); 
 		
-		app.get("/test", controller::listLibros);
-		app.post("/test", controller::addLibro);
+		app.get("/libros", controller::listLibros);
+		app.post("/libros", controller::addLibro);
 		
 	}
 	
